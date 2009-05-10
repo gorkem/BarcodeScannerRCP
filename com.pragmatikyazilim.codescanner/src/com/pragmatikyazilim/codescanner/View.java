b07e29fee439daaad88ca25733570108420afe7d
gorkem <gercan@acm.org>
2009-05-10 22:00:02 +0300

    Update copyrights

--- com.pragmatikyazilim.codescanner/src/com/pragmatikyazilim/codescanner/View.java
+++ com.pragmatikyazilim.codescanner/src/com/pragmatikyazilim/codescanner/View.java
@@ -1,3 +1,10 @@
+/*******************************************************************************
+ * Copyright (c) 2009 Gorkem Ercan.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
 package com.pragmatikyazilim.codescanner;
 
 import org.eclipse.core.runtime.CoreException;
